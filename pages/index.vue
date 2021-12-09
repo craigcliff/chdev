d3e98a3c8871578f104042243ecc955a1cf95da7
craigcliff <craigcliff@gmail.com>
2021-12-09 11:00:49 +0200

    added test.vue page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,21 @@
 <template>
+<div class="container">
   <h1>CRAIG HECTORS PLAYGROUND SITE</h1>
+</div>
 </template>
 
 <script>
 export default {}
 </script>
+
+
+<style scoped>
+
+.container {
+  display: flex;
+  background-color: black;
+  width: 50vw;
+  height: 100vh;
+}
+
+</style>
